e06f9d7d180aa9e91bbc75ff2e34a546f20acd4a
Juan <juanperrotat@gmail.com>
2026-05-28 19:06:27 -0300

    Desarrollo de Index

--- dominio/Disco.cs
+++ dominio/Disco.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,10 @@ namespace dominio
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
+        [DisplayName("Fecha de lanzamiento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FechaLanzamiento { get; set; }
+        [DisplayName("Cantidad de canciones")]
         public int CantidadCanciones { get; set; }
         public string UrlTapa { get; set; }
         public Estilo Estilo { get; set; }
